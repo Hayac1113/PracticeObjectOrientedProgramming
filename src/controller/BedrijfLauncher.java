b9fb2276e474680e72852aa6ad9976c042ae10b1
Hayac1113 <haoming.isaac@gmail.com>
2023-10-02 18:33:16 +0200

    Encapsulation in Java OOP

--- src/controller/BedrijfLauncher.java
+++ src/controller/BedrijfLauncher.java
@@ -7,15 +7,38 @@ public class BedrijfLauncher {
         System.out.println(Persoon.aantalPersonen);
         Persoon baas = new Persoon("Mark", "Den Haag", 10000);
         System.out.println(Persoon.aantalPersonen);
-        System.out.println(baas.personeelsNummer);
+        System.out.println(baas.getPersoneelsNummer());
         Persoon medewerker = new Persoon("Caroline", "Delft", 4000);
         System.out.println(Persoon.aantalPersonen);
-        System.out.println(medewerker.personeelsNummer);
+        System.out.println(medewerker.getPersoneelsNummer());
 
+        /*
+        * Verbeter de fouten, die inmiddels in de code in je main methode zijn ontstaan (benader de
+        attributen via hun getters).*/
         Persoon assistent = new Persoon ("Klaas");
         Persoon manager = new Persoon();
         System.out.println(Persoon.aantalPersonen);
-        System.out.printf("%S verdient %.2f per jaar\n", baas.naam, baas.berekenJaarInkomen());
-        System.out.printf("%s woont in %s\n", assistent.naam, assistent.woonplaats);
+        System.out.printf("%S verdient %.2f per jaar\n", baas.getNaam(), baas.berekenJaarInkomen());
+        System.out.printf("%S verdient %.2f per jaar\n", medewerker.getNaam(), medewerker.berekenJaarInkomen());
+        System.out.printf("%s woont in %s\n", assistent.getNaam(), assistent.getWoonplaats());
+
+        //If methode om te kijken of iemand recht heeft op bonus
+        if (baas.heeftRechtOpBonus()) {
+            System.out.printf("%s heeft recht op bonus\n", baas.getNaam());
+        } else {
+            System.out.printf("%s heeft geen recht op bonus\n", baas.getNaam());
+        }
+
+        if (medewerker.heeftRechtOpBonus()) {
+            System.out.printf("%s heeft recht op bonus\n", medewerker.getNaam());
+        } else {
+            System.out.printf("%s heeft geen recht op bonus\n", medewerker.getNaam());
+        }
+
+        if (assistent.heeftRechtOpBonus()) {
+            System.out.printf("%s heeft recht op bonus\n", assistent.getNaam());
+        } else {
+            System.out.printf("%s heeft geen recht op bonus\n", assistent.getNaam());
+        }
     }
 }
--- src/model/Persoon.java
+++ src/model/Persoon.java
@@ -2,11 +2,11 @@ package model;
 
 public class Persoon {
     public static int aantalPersonen;
-    public final int personeelsNummer;
-    public String naam;
-    public String woonplaats;
-    public double maandSalaris;
-    public final double GRENSWAARDE_BONUS = 4500.00;
+    private final int personeelsNummer;
+    private String naam;
+    private String woonplaats;
+    private double maandSalaris;
+    private static final double GRENSWAARDE_BONUS = 4500.00;
 
     public Persoon(String naam, String woonplaats, double maandSalaris) {
         this.personeelsNummer = ++aantalPersonen;
@@ -36,4 +36,63 @@ public class Persoon {
     public boolean heeftRechtOpBonus() {
         return maandSalaris > GRENSWAARDE_BONUS;
     }
+
+    /*
+    * a) Voeg de constante GRENSWAARDE_BONUS toe.
+    b) Maak de vier niet-statische attributen private.
+    c) Voeg getters en setters toe voor deze vier attributen (de getters en setters zijn niet
+    opgenomen in het klassendiagram). Let wel: voor het attribuut personeelsNummer mag je
+    geen setter opnemen. Dit attribuut is final en kun je dus niet meer aanpassen.
+    d) Pas de methode setMaandSalaris() aan, zodat deze een foutmelding geeft als het
+    meegegeven bedrag negatief is. Het maandsalaris wordt dan op 0 gezet.
+    e) Pas constructor chaining toe op de drie constructors.
+    f) Zorg dat ook in de constructors geen negatief maandsalaris geaccepteerd wordt.
+    g) Voeg de methode heeftRechtOpBonus() toe. Deze geeft true als het maandsalaris
+    groter is dan of gelijk is aan de GRENSWAARDE_BONUS.*/
+public int getPersoneelsNummer() {
+        return personeelsNummer;
+    }
+
+    public String getNaam() {
+        return naam;
+    }
+
+    public void setNaam(String naam) {
+        this.naam = naam;
+    }
+
+    public String getWoonplaats() {
+        return woonplaats;
+    }
+
+    public void setWoonplaats(String woonplaats) {
+        this.woonplaats = woonplaats;
+    }
+
+    public double getMaandSalaris() {
+        return maandSalaris;
+    }
+
+    public void setMaandSalaris(double maandSalaris) {
+        if (maandSalaris < 0) {
+            System.out.println("Het maandsalaris mag niet negatief zijn. Het wordt op 0 gezet.");
+            this.maandSalaris = 0.0;
+        } else {
+            this.maandSalaris = maandSalaris;
+        }
+    }
+
+    public static double getGrensWaardeBonus() {
+        return GRENSWAARDE_BONUS;
+    }
+
+    public boolean equals(Persoon persoon) {
+        return this.personeelsNummer == persoon.personeelsNummer;
+    }
+
+    public String toString() {
+        return String.format("%s (%d)", naam, personeelsNummer);
+    }
+
+
 }
